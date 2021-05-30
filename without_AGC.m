7e1200c2a3b768f2acccc67ec51fd298effb150c
Ganesh Rai <72004365+GRxGOD@users.noreply.github.com>
2021-05-30 13:27:03 +0530

    Update without_AGC.m

--- without_AGC.m
+++ without_AGC.m
@@ -3,5 +3,5 @@ den = [1 7.08 10.56 20.8];
 t = 0:.02:10;
 c = -pl*step(num,den,t);
 plot(t, c), xlabel("t, sec"), ylabel("pu")
-title("Frequency deviation step response"),grid
+title("Frequency Response"),grid
 timespec(num, den)
